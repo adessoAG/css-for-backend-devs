7701857d0301805674f320104242e5efb6444c56
Priyanka Shete <piyukore06@gmail.com>
2020-08-31 09:15:18 +0200

    Updated intro slide

--- src/1.Intro/2.1.Intro.jsx
+++ src/1.Intro/2.1.Intro.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, TableRow, TableCell, UnorderedList, ListItem, Image } from 'spectacle';
+import { Table, TableRow, TableCell, UnorderedList, ListItem, Image, Heading } from 'spectacle';
 import './image.css';
 
 function SubHeading() {
@@ -7,28 +7,31 @@ function SubHeading() {
         <div>
             <Table>
                 <TableRow>
-                    <TableCell width="50%">
-                        <UnorderedList>
-                            <ListItem>Frontend-Architekt bei Adesso</ListItem>
-                            <ListItem>Leiter eines JavaScript Competence Centers</ListItem>
-                            <ListItem>16 Jahre in der Web-entwicklung</ListItem>
-                        </UnorderedList>
-                    </TableCell>
                     <TableCell>
                         <Image className="hori-centered avatar" src="frontend/konstantin.png"></Image>
                     </TableCell>
+                    <TableCell width="50%">
+                        <Heading fontSize="text">Priyanka Shete</Heading>
+                        <UnorderedList>
+                            <ListItem>Senior Software Engineer bei Adesso.</ListItem>
+                            <ListItem>Speaker, Writer.</ListItem>
+                            <ListItem>5 Jahre in der Web-entwicklung.</ListItem>
+                            <ListItem>Twittert unter @piyukore06.</ListItem>
+                        </UnorderedList>
+                    </TableCell>
+
                 </TableRow>
                 <TableRow>
+                    <TableCell width="50%">
+                        <Heading fontSize="text">Konstantin Pentarakis</Heading>
+                        <UnorderedList>
+                            <ListItem>Frontend-Architekt bei Adesso.</ListItem>
+                            <ListItem>Leiter eines JavaScript Competence Centers.</ListItem>
+                            <ListItem>16 Jahre in der Web-entwicklung.</ListItem>
+                        </UnorderedList>
+                    </TableCell>
                     <TableCell>
                         <Image className="hori-centered avatar" src="frontend/priyanka.jpg"></Image>
-                    </TableCell>
-                    <TableCell width="50%">
-                        <UnorderedList>
-                            <ListItem>Senior Software Engineer bei Adesso</ListItem>
-                            <ListItem>Speaker, Writer</ListItem>
-                            <ListItem>5 Jahre in der Web-entwicklung</ListItem>
-                            <ListItem>Twittert unter @piyukore06</ListItem>
-                        </UnorderedList>
                     </TableCell>
                 </TableRow>
             </Table>
